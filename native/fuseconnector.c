92a72183112e2819ed485b1c2ce0db134bec9cfd
Warren Falk <warren@warrenfalk.com>
2012-02-11 13:46:38 -0500

    successfully passed java string array to native side

--- native/fuseconnector.c
+++ native/fuseconnector.c
@@ -10,6 +10,20 @@
 
 JNIEXPORT jint JNICALL Java_warrenfalk_fuselaj_Fuselaj_initialize (JNIEnv *env, jobject obj, jobjectArray args) {
 	int argc;
+	const char **argv;
+	int i;
 	argc = (*env)->GetArrayLength(env, args);
+	argv = (const char**)malloc(sizeof(char*) * argc);
+	for (i = 0; i < argc; i++) {
+		jstring str = (jstring)(*env)->GetObjectArrayElement(env, args, i);
+		argv[i] = (*env)->GetStringUTFChars(env, str, 0);
+	}
 	printf("argc = %d\n", argc);
+	for (i = 0; i < argc; i++) {
+		printf("  arg %d: %s\n", i, argv[i]);
+	}
+	for (i = 0; i < argc; i++) {
+		jstring str = (jstring)(*env)->GetObjectArrayElement(env, args, i);
+		(*env)->ReleaseStringUTFChars(env, str, argv[i]);
+	}
 }
