e71dc8a99ecc46b91c9aebee0cdb70f68e4f68b8
Warren Falk <warren@warrenfalk.com>
2012-08-04 12:34:06 -0400

    Added documentation for FileInfo open flags (O_FLAGS)

--- src/warrenfalk/fuselaj/FileInfo.java
+++ src/warrenfalk/fuselaj/FileInfo.java
@@ -6,18 +6,51 @@ import java.nio.ByteOrder;
 public final class FileInfo {
 	final ByteBuffer bb;
 	
+	/** A mask for the read/write flags **/
 	public final static int O_ACCMODE = 0003;
+	/** open for reading only **/
 	public final static int O_RDONLY = 00;
+	/** open for writing only **/
 	public final static int O_WRONLY = 01;
-	public final static int O_RDWR = 02;	
+	/** open for reading and writing **/
+	public final static int O_RDWR = 02;
+	/** If the file exists, this flag has no effect except as noted under O_EXCL.
+	 * Otherwise the file is created; 
+	 * the user ID of the file is set to the effective user ID of the process; 
+	 * the group ID of the file is set to the group ID of the file's parent directory or to the effective group ID of the process;
+	 * and the access permission bits (see sys/stat.h) of the file mode are set to the value of the third argument taken as type mode_t (int) modified as follows:
+	 *   a bitwise-AND is performed on the file-mode bits and the corresponding bits in the comlement of the process' file mode creation mask.
+	 *   Thus all bits in the file mode whose corresponding bit in the file mode creating mask is set are cleared.
+	 *   When bits other than the file permission bits are set, the effect is unspecified.  The third argument does not affect whether the file is open for reading, writing or both.
+	 */
 	public final static int O_CREAT = 0100;
+	/** If O_CREAT and O_EXCL are set, open() will fail if the file exists.
+	 * The check for the existence of the file and the creation of the file if it does not exist will be atomic with respect to other processes executing open() nameing the same filename in the same directory with O_EXCL and O_CREAT set.
+	 * If O_CREAT is not set, the effect is undefined. 
+	 */
 	public final static int O_EXCL = 0200;
+	/** If set and path identifies a terminal device, open() will not cause the terminal device to become the controlling terminal for the process **/
 	public final static int O_NOCTTY = 0400;
+	/** If the file exists and is a regular file and the file is successfully opened O_RDWR or O_WRONLY, its length is truncated to 0 and the mode and owner are unchanged.
+	 * It will have no effect on FIFO special files or terminal device files.
+	 * Its effect on other types of is implementation-dependent.
+	 * The result of using O_TRUNC with O_RDONLY is undefined. 
+	 * If O_TRUNC is set and the file did previously exist, upon successful comlpetion, open() will mark for update the st_ctime and st_mtime fields of the file **/
 	public final static int O_TRUNC = 01000;
 	public final static int O_APPEND = 02000;
+	/** When opening a FIFO with O_RDONLY or O_WRONLY set, if O_NONBLOCK is set, an open() for reading only will return without delay.  An open() for writing only will return an error if no process currently has the file open for reading.
+	 * If O_NONBLOCK is clear, an open() for reading only will block the calling thread until a thread opens the file for writing.  An open() for writing only will block the calling thread until a thread opens the file for reading.
+	 * When opening a block special or character special file that supports non-blocking opens:
+	 * If O_NONBLOCK is set, the open() function will return without blocking for the device to be ready or available; subsequent behavior of the device is device-specific
+	 * If O_NONBLOCK is clear, the open() function will block the calling thread until the device is ready or available before returning.
+	 * Otherwise, the behavior of O_NONBLOCK is unspecified.
+	 */
 	public final static int O_NONBLOCK = 04000;
+	/** see O_NONBLOCK **/
 	public final static int O_NDELAY = O_NONBLOCK;
+	/** Write I/O operations on the file descriptor complete as defined by synchronous I/O file integrity completion **/ 
 	public final static int O_SYNC = 04010000;
+	/** see O_SYNC **/
 	public final static int O_FSYNC = O_SYNC;
 	public final static int O_ASYNC = 020000;
 	
@@ -32,9 +65,9 @@ public final class FileInfo {
 	/* Do not set atime. */
 	public final static int O_NOATIME = 01000000;
 	
-	/* Synchronize data. */
+	/** Write I/O operations on the file descriptor complete as defined by the synchronized I/O data integrity completion **/
 	public final static int O_DSYNC = 010000;
-	/* Synchronize read operations. */
+	/** see O_SYNC **/
 	public final static int O_RSYNC = O_SYNC;
 	
 	public final static int O_LARGEFILE = 0100000;
